1fec632a83ff3064279560280141e79f671cc1d1
Noel Guillen <noelblas274@gmail.com>
2024-06-17 14:16:01 -0400

    numeric operations work added

---
+++ numbers/app.py
@@ -0,0 +1,9 @@
+x = 1  # number
+x = 1.1  # double
+x = 1 + 2
+
+10 // 3  # round
+10 % 3  # remainder
+
+10 ** 3  # 10 to the power of 3
+print(10 ** 3)
---
+++ numbers/typeConversion.py
@@ -0,0 +1,14 @@
+x = input("x: ")
+print(type(x))
+y = int(x) + 1
+print(f"x: {x}, y: {y}")
+int(x)
+float(x)
+bool(x)
+str(x)  # convert to string
+
+# falsy values
+
+# ""
+# 0
+# none
