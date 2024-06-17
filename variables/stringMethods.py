3902efb11261592505a7c3f0eab0e3e11fc9cf7b
Noel Guillen <noelblas274@gmail.com>
2024-06-17 14:00:17 -0400

    string methods

--- variables/stringMethods.py
+++ variables/stringMethods.py
@@ -1,5 +1,11 @@
-course = "Python Programming"
-
-course.capitalize
-
-print(course)
+course = " python programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.lstrip())
+print(course.rstrip())
+print(course.find("pro"))
+print(course.replace("p", "j"))
+print("pro" in course)
+print("swift" not in course)
