512004ba3655897534f5346595b22d26606622a5
Noel Guillen <noelblas274@gmail.com>
2024-06-17 15:56:27 -0400

    loop section with mod exercise

---
+++ loops/app.py
@@ -0,0 +1,17 @@
+
+# for i in range(3):
+#     print("Attempt",(i+1), (i + 1) * ".")
+
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
+
+for i in "Noel":
+    print(i)
+
+num = 0
+
+while num < 5:
+    print(num)
+    num += 2
+
